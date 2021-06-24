70b8652e33bd1bd25fb97c0796cf85c3befba54a
Talia Pichardo <taliapichardo@gmail.com>
2021-06-24 18:39:25 -0400

    Edit home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
         <Container>
             <Jumbotron className='text-center bg-warning mt-5'>
                 <h1>Welcome to Bread!</h1>
-                <p>Stuck between <strong>"I need to save money"</strong> and <strong>"you only live once?"</strong></p>
+                <p>Stuck between <strong>"I need to save money"</strong> and <strong>"you only live once"</strong>?</p>
                 <h6>Start budgeting your money today with Bread, the best budgeting app!</h6>
                 <br />
                 <Link to='/transactions/new'>
