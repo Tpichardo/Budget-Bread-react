74437b825b52cbce01f3fc6a4736ec60a130f7b0
Talia Pichardo <taliapichardo@gmail.com>
2022-03-17 01:16:05 -0400

    Adds newTransactionForm directory

--- src/Pages/New.js
+++ src/Pages/New.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import NewTransactionForm from '../Components/NewTransactionForm';
+import NewTransactionForm from '../Components/newTransactionForm/NewTransactionForm';
 
 function New({ addTransaction }) {
     return (
