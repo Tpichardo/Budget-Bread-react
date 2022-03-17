7615356baac973d67389ecf37a93fd2218b104b9
Talia Pichardo <taliapichardo@gmail.com>
2022-03-17 03:07:13 -0400

    Adds unit test to check if input values appear on thenewTransactionForme form

--- src/Components/newTransactionForm/__test__/NewTransactionForm.test.js
+++ src/Components/newTransactionForm/__test__/NewTransactionForm.test.js
@@ -1,6 +1,7 @@
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import NewTransactionForm from '../NewTransactionForm.js';
+
 
 //Wraps NewTransactionForm in BrowserRouter so that withRouter is rendered within BrowserRouter
 const MockForm = () => {
@@ -11,7 +12,40 @@ const MockForm = () => {
     )
 }
 
-test('renders new transaction form', () => {
-    render(<MockForm />);
+describe("A form that displays different inputs", () => {
+
+    test("renders new transaction form", () => {
+        render(<MockForm />);
+    });
+
+    test("renders date input element", () => {
+        const { getByLabelText } = render(<MockForm addTransaction={[]} />);
+        const date = getByLabelText("Date");
+
+        expect(date).toBeInTheDocument();
+    });
+
+    test("renders name input element", () => {
+        const { getByLabelText } = render(<MockForm addTransaction={[]} />);
+        const input = getByLabelText("Name");
+
+        expect(input).toBeInTheDocument();
+    });
+
+    test("renders amount input element", () => {
+        const { getByLabelText } = render(<MockForm addTransaction={[]} />);
+        const amount = getByLabelText("Amount");
+
+        expect(amount).toBeInTheDocument();
+    });
+
+    test("renders text area element", () => {
+        const { getByLabelText } = render(<MockForm addTransaction={[]} />);
+        const from = getByLabelText("From");
+
+        expect(from).toBeInTheDocument();
+    });
+
 });
+
 
