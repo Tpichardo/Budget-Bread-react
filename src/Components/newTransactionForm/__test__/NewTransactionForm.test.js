2fb9a179b6496ae9943fc2228ff03fad93e8605d
Talia Pichardo <taliapichardo@gmail.com>
2022-03-17 04:22:34 -0400

    Adds fireEvent to fire event handler and test newTransaction form input elements

--- src/Components/newTransactionForm/__test__/NewTransactionForm.test.js
+++ src/Components/newTransactionForm/__test__/NewTransactionForm.test.js
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import NewTransactionForm from '../NewTransactionForm.js';
 
@@ -12,11 +12,13 @@ const MockForm = () => {
     )
 }
 
-describe("A form that displays different inputs", () => {
+describe("A form that allows the user to input a date, name, amount, and from value", () => {
 
     test("renders new transaction form", () => {
         render(<MockForm />);
     });
+
+    //Date input element
 
     test("renders date input element", () => {
         const { getByLabelText } = render(<MockForm addTransaction={[]} />);
@@ -25,13 +27,23 @@ describe("A form that displays different inputs", () => {
         expect(date).toBeInTheDocument();
     });
 
+    //Name input element
     test("renders name input element", () => {
         const { getByLabelText } = render(<MockForm addTransaction={[]} />);
-        const input = getByLabelText("Name");
+        const transactionName = getByLabelText("Name");
 
-        expect(input).toBeInTheDocument();
+        expect(transactionName).toBeInTheDocument();
     });
 
+    test("allows user to type in name input", () => {
+        const { getByLabelText } = render(<MockForm addTransaction={[]} />)
+        const transactionNameInput = getByLabelText("Name");
+        fireEvent.change(transactionNameInput, { target: { value: "Dinner with Mami" } });
+
+        expect(transactionNameInput.value).toBe("Dinner with Mami");
+    });
+
+    //Amount input element
     test("renders amount input element", () => {
         const { getByLabelText } = render(<MockForm addTransaction={[]} />);
         const amount = getByLabelText("Amount");
@@ -39,21 +51,37 @@ describe("A form that displays different inputs", () => {
         expect(amount).toBeInTheDocument();
     });
 
-    test("renders text area element", () => {
-        const { getByLabelText } = render(<MockForm addTransaction={[]} />);
-        const from = getByLabelText("From");
-
-        expect(from).toBeInTheDocument();
-    });
-
-});
-
-describe("renders a type number input", () => {
     test("renders a type number input that only accepts numbers", () => {
         const { getByLabelText } = render(<MockForm addTransaction={[]} />);
         const amount = getByLabelText("Amount")
 
         expect(amount).not.toBeNaN();
     });
-})
+
+    test("allows user to enter a number in amount input", () => {
+        const { getByLabelText } = render(<MockForm addTransaction={[]} />);
+        const amount = getByLabelText("Amount");
+        fireEvent.change(amount, { target: { value: "80" } });
+
+        expect(amount.value).toBe("80");
+    });
+
+    //Textarea element
+    test("renders text area element", () => {
+        const { getByLabelText } = render(<MockForm addTransaction={[]} />);
+        const transactionDescription = getByLabelText("From");
+
+        expect(transactionDescription).toBeInTheDocument();
+    });
+
+    test("allows user to enter a transaction description in textarea", () => {
+        const { getByLabelText } = render(<MockForm addTransaction={[]} />);
+        const transactionDescription = getByLabelText("From");
+        fireEvent.change(transactionDescription, { target: { value: "Mama Sushi" } });
+
+        expect(transactionDescription.value).toBe("Mama Sushi");
+    });
+
+});
+
 
