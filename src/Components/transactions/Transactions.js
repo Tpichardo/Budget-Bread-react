5292a323ceee35b8ea6c17f6fc802655ca56b549
Talia Pichardo <taliapichardo@gmail.com>
2022-03-18 02:36:02 -0400

    Refactors transaction component, adding API call to useEffect and asynchronous loading component

---
+++ src/Components/transactions/Transactions.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react'
+import { apiURL } from '../../util/apiURL';
+import Transaction from '../Transaction'
+import LoadingView from '../loadingView/LoadingView';
+import Container from 'react-bootstrap/Container';
+
+const API = apiURL();
+
+const Transactions = () => {
+    const [transactions, setTransactions] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        axios.get(`${API}/transactions`).then((response) => {
+            const { data } = response;
+            setTransactions(data);
+            setLoading(false)
+        }).catch((e) => {
+            console.log(e)
+        });
+    }, []);
+
+
+
+    let total = 0
+    transactions.forEach(transaction => {
+        total += Number(transaction.amount)
+    })
+
+    return (
+        <div>
+            {loading && <LoadingView />}
+
+            {!loading &&
+                <Container>
+                    {total > 1000 ?
+                        <h1 style={{ color: "green", backgroundColor: "#ffffff" }}>Bank Account Total: ${total.toLocaleString("en-US", { minimumFractionDigits: 2 })}</h1>
+                        : <h1 style={{ color: "red", backgroundColor: "#ffffff" }}>Bank Account Total: ${total.toLocaleString('en-US', { minimumDecimalFractions: 2 })}</h1>}
+                </Container>
+
+            };
+
+            {!loading &&
+                transactions.map((transaction, index) => {
+                    return (
+                        <div>
+                            <Transaction key={index} transaction={transaction} index={index} />
+                        </div>
+                    );
+                })}
+
+        </div>
+    )
+}
+
+export default Transactions;
