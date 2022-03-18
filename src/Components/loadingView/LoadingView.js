24f3262813b1658cf036a71b87e6adafc294b239
Talia Pichardo <taliapichardo@gmail.com>
2022-03-18 02:39:33 -0400

    Adds asynchronous component, loadingView, as well as style and unit test to ensure component displays loading key word

---
+++ src/Components/loadingView/LoadingView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LoadingView.scss';
+
+const LoadingView = () => {
+    return (
+        <div className='loadingView'>
+            <h1>Loading Transactions...</h1>
+        </div>
+    )
+}
+
+export default LoadingView;
