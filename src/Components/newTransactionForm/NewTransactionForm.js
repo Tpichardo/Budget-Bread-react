3b38cb82ffe1ecfc4f97e67a75d2813e7524f561
Talia Pichardo <taliapichardo@gmail.com>
2022-03-20 19:20:18 -0400

    adds card component to newTransaction form

--- src/Components/newTransactionForm/NewTransactionForm.js
+++ src/Components/newTransactionForm/NewTransactionForm.js
@@ -3,10 +3,7 @@ import axios from 'axios';
 import { useState } from 'react';
 import { withRouter, useHistory } from 'react-router-dom';
 import { apiURL } from '../../util/apiURL';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Container from 'react-bootstrap/Container';
+import { Container, Form, Button, InputGroup, Card } from 'react-bootstrap';
 
 
 function NewTransactionForm() {
@@ -41,67 +38,72 @@ function NewTransactionForm() {
 
     return (
         <Container>
-            <h1>Add New Transaction</h1>
-            <Form onSubmit={handleSubmit}>
-                <Form.Group controlId="date">
-                    <Form.Label>Date</Form.Label>
-                    <Form.Control
-                        value={transaction.date}
-                        type="date"
-                        required
-                        onChange={handleChange}
-                        placeholder="Date"
-                    />
-                </Form.Group>
+            <Card>
+                <Card.Body>
 
-                <Form.Group controlId="name">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control
-                        value={transaction.name}
-                        type="text"
-                        required
-                        onChange={handleChange}
-                        placeholder="Name"
-                    />
-                </Form.Group>
+                    <h1>Add New Transaction</h1>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group controlId="date">
+                            <Form.Label>Date</Form.Label>
+                            <Form.Control
+                                value={transaction.date}
+                                type="date"
+                                required
+                                onChange={handleChange}
+                                placeholder="Date"
+                            />
+                        </Form.Group>
 
-                <Form.Group>
-                    <Form.Label>Amount</Form.Label>
-                    <InputGroup className="mb-3">
-                        <InputGroup.Prepend>
-                            <InputGroup.Text id="amount">$</InputGroup.Text>
-                        </InputGroup.Prepend>
-                        <Form.Control
-                            aria-label="Amount"
-                            id="amount"
-                            value={transaction.amount}
-                            type="number"
-                            required
-                            onChange={handleChange}
-                        />
-                        <InputGroup.Append>
-                            <InputGroup.Text>.00</InputGroup.Text>
-                        </InputGroup.Append>
-                    </InputGroup>
-                </Form.Group>
+                        <Form.Group controlId="name">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control
+                                value={transaction.name}
+                                type="text"
+                                required
+                                onChange={handleChange}
+                                placeholder="Name"
+                            />
+                        </Form.Group>
 
-                <Form.Group controlId="from">
-                    <Form.Label>From</Form.Label>
-                    <Form.Control
-                        value={transaction.from}
-                        type="text"
-                        required
-                        onChange={handleChange}
-                        placeholder="Description of where transaction came from"
-                        as="textarea"
-                        rows={3}
-                    />
-                </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Amount</Form.Label>
+                            <InputGroup className="mb-3">
+                                <InputGroup.Prepend>
+                                    <InputGroup.Text id="amount">$</InputGroup.Text>
+                                </InputGroup.Prepend>
+                                <Form.Control
+                                    aria-label="Amount"
+                                    id="amount"
+                                    value={transaction.amount}
+                                    type="number"
+                                    required
+                                    onChange={handleChange}
+                                />
+                                <InputGroup.Append>
+                                    <InputGroup.Text>.00</InputGroup.Text>
+                                </InputGroup.Append>
+                            </InputGroup>
+                        </Form.Group>
 
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
+                        <Form.Group controlId="from">
+                            <Form.Label>From</Form.Label>
+                            <Form.Control
+                                value={transaction.from}
+                                type="text"
+                                required
+                                onChange={handleChange}
+                                placeholder="Description of where transaction came from"
+                                as="textarea"
+                                rows={3}
+                            />
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </Card.Body>
+            </Card>
         </Container>
     )
 }
