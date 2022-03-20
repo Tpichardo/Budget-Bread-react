49db34cb65b00433de0eda68981f060903143770
Talia Pichardo <taliapichardo@gmail.com>
2022-03-20 18:06:35 -0400

    Refactors firebase code

---
+++ src/util/firebase.js
@@ -0,0 +1,37 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+
+const {
+    REACT_APP_FIREBASE_API_KEY,
+    REACT_APP_FIREBASE_AUTH_DOMAIN,
+    REACT_APP_FIREBASE_PROJECT_ID,
+    REACT_APP_FIREBASE_STORAGE_BUCKET,
+    REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    REACT_APP_FIREBASE_APP_ID,
+    REACT_APP_FIREBASE_MEASUREMENT_ID
+} = process.env;
+
+const firebaseConfig = {
+    apiKey: REACT_APP_FIREBASE_API_KEY,
+    authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: REACT_APP_FIREBASE_APP_ID,
+    measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export const signUp = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password)
+};
+
+
+
