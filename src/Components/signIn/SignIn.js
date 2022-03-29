684e295fec602746956821d5438f45061def57dc
Talia Pichardo <taliapichardo@gmail.com>
2022-03-28 20:09:33 -0400

    Adds bootstrap container to signUp and signIn forms

--- src/Components/signIn/SignIn.js
+++ src/Components/signIn/SignIn.js
@@ -6,33 +6,35 @@ import './SignIn.scss'
 
 const SignIn = () => {
     return (
-        <Card className='signIn'>
-            <h3 className='signIn__greeting'>Welcome Back!</h3>
-            <Card.Body>
-                <Form>
-                    <Form.Group>
-                        <Form.Label>Email:</Form.Label>
-                        <Form.Control
-                            type='email'
-                            placeholder='example@gmail.com'
-                            required
-                        />
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Label>Password:</Form.Label>
-                        <Form.Control
-                            type='password'
-                            placeholder='password'
-                            rewuired
-                        />
-                    </Form.Group>
-                    <div className='signIn__BtnDiv'>
-                        <Button className='signIn__Btn' type='submit' variant='primary'>Log In</Button>
-                    </div>
-                </Form>
-            </Card.Body>
-            <p className='signIn__signUp'>Don't have an account? <Link to='/signup' k>Sign Up</Link></p>
-        </Card>
+        <Container>
+            <Card className='signIn'>
+                <h3 className='signIn__greeting'>Welcome Back!</h3>
+                <Card.Body>
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>Email:</Form.Label>
+                            <Form.Control
+                                type='email'
+                                placeholder='example@gmail.com'
+                                required
+                            />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Password:</Form.Label>
+                            <Form.Control
+                                type='password'
+                                placeholder='password'
+                                rewuired
+                            />
+                        </Form.Group>
+                        <div className='signIn__BtnDiv'>
+                            <Button className='signIn__Btn' type='submit' variant='primary'>Log In</Button>
+                        </div>
+                    </Form>
+                </Card.Body>
+                <p className='signIn__signUp'>Don't have an account? <Link to='/signup' k>Sign Up</Link></p>
+            </Card>
+        </Container>
     );
 }
 
--- src/Components/signUp/SignUp.js
+++ src/Components/signUp/SignUp.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useRef, useState } from 'react';
 import { useUserContext } from '../../context/UserContext.js';
 import { Link } from 'react-router-dom';
-import { Card, Form, Button, Alert } from 'react-bootstrap';
+import { Card, Form, Button, Alert, Container } from 'react-bootstrap';
 import { auth } from '../../util/firebaseConfig.js'
 import { updateProfile } from 'firebase/auth';
 import './SignUp.scss';
@@ -56,57 +56,57 @@ const SignUp = () => {
     };
 
     return (
-        <Card className='signUp'>
-            <Card.Body>
-                <h3 className='signUp__greeting'>Sign Up</h3>
-                {error && <Alert variant='danger'>{error}</Alert>}
-                <Form onSubmit={handleSubmit}>
-                    <Form.Group controlId='displayName'>
-                        <Form.Label>Name:</Form.Label>
-                        <Form.Control
-                            type='text'
-                            ref={nameRef}
-                            placeholder="name"
-                            required
-                        />
-                    </Form.Group>
-                    <Form.Group controlId='email'>
-                        <Form.Label>Email:</Form.Label>
-                        <Form.Control
-                            type='email'
-                            ref={emailRef}
-                            placeholder="example@gmail.com"
-                            required
-                        />
-                    </Form.Group>
-                    <Form.Group controlId='password'>
-                        <Form.Label>Password:</Form.Label>
-                        <Form.Control
-                            type='password'
-                            ref={passwordRef}
-                            placeholder='password must be at least 6 characters'
-                            required
-                        />
-                    </Form.Group>
-                    <Form.Group controlId='confirmPassword'>
-                        <Form.Label>Confirm Password:</Form.Label>
-                        <Form.Control
-                            type='password'
-                            ref={confirmPasswordRef}
-                            placeholder="confirm password"
-                            required
-                        />
-                    </Form.Group>
-                    <div className="signUp__BtnDiv">
-                        <Button className='signUp__Btn' disabled={loading} type='submit' variant='primary'>Sign Up</Button>
-                    </div>
-                </Form>
-                <p className='signUp__signIn'>Already have an account? <Link to='/signin' className='authenticate'>Sign in</Link></p>
-            </Card.Body>
-        </Card>
-
-
-    )
+        <Container>
+            <Card className='signUp'>
+                <Card.Body>
+                    <h3 className='signUp__greeting'>Sign Up</h3>
+                    {error && <Alert variant='danger'>{error}</Alert>}
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group controlId='displayName'>
+                            <Form.Label>Name:</Form.Label>
+                            <Form.Control
+                                type='text'
+                                ref={nameRef}
+                                placeholder="name"
+                                required
+                            />
+                        </Form.Group>
+                        <Form.Group controlId='email'>
+                            <Form.Label>Email:</Form.Label>
+                            <Form.Control
+                                type='email'
+                                ref={emailRef}
+                                placeholder="example@gmail.com"
+                                required
+                            />
+                        </Form.Group>
+                        <Form.Group controlId='password'>
+                            <Form.Label>Password:</Form.Label>
+                            <Form.Control
+                                type='password'
+                                ref={passwordRef}
+                                placeholder='password must be at least 6 characters'
+                                required
+                            />
+                        </Form.Group>
+                        <Form.Group controlId='confirmPassword'>
+                            <Form.Label>Confirm Password:</Form.Label>
+                            <Form.Control
+                                type='password'
+                                ref={confirmPasswordRef}
+                                placeholder="confirm password"
+                                required
+                            />
+                        </Form.Group>
+                        <div className="signUp__BtnDiv">
+                            <Button className='signUp__Btn' disabled={loading} type='submit' variant='primary'>Sign Up</Button>
+                        </div>
+                    </Form>
+                    <p className='signUp__signIn'>Already have an account? <Link to='/signin' className='authenticate'>Sign in</Link></p>
+                </Card.Body>
+            </Card>
+        </Container>
+    );
 }
 
 export default SignUp;
