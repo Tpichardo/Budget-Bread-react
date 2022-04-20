859a1c0c3dd8918d3c7f37f5ea4bf0b4c5e8af59
Talia Pichardo <taliapichardo@gmail.com>
2022-04-19 23:17:22 -0400

    Adds additional styling to transaction table

--- src/Components/Transaction.js
+++ src/Components/Transaction.js
@@ -2,26 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Table from 'react-bootstrap/Table';
 import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card'
 
 const Transaction = ({ transaction, index }) => {
     return (
         <Container>
-            <Table striped hover size="sm" className='bg-light' >
-                <thead>
-                    <tr>
-                        <th className='col-1'>Date</th>
-                        <th className='col-1 text-center'>Name</th>
-                        <th className='col-1 text-center'>Amount</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>{transaction.date}</td>
-                        <td className='text-center'><Link to={`/transactions/${index}`}>{transaction.name}</Link></td>
-                        {transaction.amount > 0 ? <td className='text-center text-success'>{Number(transaction.amount).toFixed(2)}</td> : <td className='text-center text-danger'>{Number(transaction.amount).toFixed(2)}</td>}
-                    </tr>
-                </tbody>
-            </Table>
+            <Card>
+                <Table striped bordered hover size="sm">
+                    <thead>
+                        <tr>
+                            <th className='col-1'>Date</th>
+                            <th className='col-1 text-center'>Name</th>
+                            <th className='col-1 text-center'>Amount</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{transaction.transactionDate}</td>
+                            <td className='text-center'><Link to={`/transactions/${index}`}>{transaction.transactionName}</Link></td>
+                            {transaction.type === 'Deposit' ? <td className='text-center text-success'>+{Number(transaction.amount).toFixed(2)}</td> : <td className='text-center text-danger'>-{Number(transaction.amount).toFixed(2)}</td>}
+                        </tr>
+                    </tbody>
+                </Table>
+            </Card>
         </Container>
     )
 }
