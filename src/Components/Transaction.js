f77a1a51581f864d9ecf6001dece51da79d4d6ff
Talia Pichardo <taliapichardo@gmail.com>
2022-04-25 01:08:02 -0400

    Updates transaction.js to use transaction id instead of index to link user to show route

--- src/Components/Transaction.js
+++ src/Components/Transaction.js
@@ -4,7 +4,7 @@ import Table from 'react-bootstrap/Table';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card'
 
-const Transaction = ({ transaction, index }) => {
+const Transaction = ({ transaction }) => {
     return (
         <Container>
             <Card>
@@ -19,7 +19,7 @@ const Transaction = ({ transaction, index }) => {
                     <tbody>
                         <tr>
                             <td>{transaction.transaction_date}</td>
-                            <td className='text-center'><Link to={`/transactions/${index}`}>{transaction.transaction_name}</Link></td>
+                            <td className='text-center'><Link to={`/transactions/${transaction.id}`}>{transaction.transaction_name}</Link></td>
                             {transaction.transaction_type === 'Deposit' ? <td className='text-center text-success'>+{Number(transaction.transaction_amount).toFixed(2)}</td> : <td className='text-center text-danger'>-{Number(transaction.transaction_amount).toFixed(2)}</td>}
                         </tr>
                     </tbody>
