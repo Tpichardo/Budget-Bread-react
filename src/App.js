4446e386f9e2c2b4a16de03c0e33a9c4d1412bd2
Talia Pichardo <taliapichardo@gmail.com>
2022-04-25 01:05:51 -0400

    Removes co mments and changes show and edit route to use :id wi ldcard intead of :index

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-//DEPENDECIES
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.scss'
 
-//BOOSTRAP
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-//Pages
 import Home from './Pages/Home.js';
 import Index from './Pages/Index.js';
 import New from './Pages/New.js';
 import Show from './Pages/Show.js';
 import Edit from './Pages/Edit.js';
 import Four0Four from './Pages/Four0Four.js';
+
 import SignIn from './Components/SignIn.js';
 import SignUp from './Components/SignUp.js';
-
-//COMPONENTS
 import NavBar from './Components/NavBar.js';
-import User from './Components/User.js';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
 
 function App() {
   return (
@@ -36,19 +30,16 @@ function App() {
           <Route path='/signup'>
             <SignUp />
           </Route>
-          <Route path='/user'>
-            <User />
-          </Route>
           <Route exact path='/transactions'>
             <Index />
           </Route>
           <Route path='/transactions/new'>
             <New />
           </Route>
-          <Route exact path='/transactions/:index'>
+          <Route exact path='/transactions/:id'>
             <Show />
           </Route>
-          <Route path='/transactions/:index/edit'>
+          <Route path='/transactions/:id/edit'>
             <Edit />
           </Route>
           <Route path='*'>
@@ -58,6 +49,6 @@ function App() {
       </Router>
     </div>
   );
-}
+};
 
 export default App;
