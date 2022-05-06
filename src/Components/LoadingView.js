b5d0a7ebf3cccce9c5090c59388c901f0bb3d2cc
Talia Pichardo <taliapichardo@gmail.com>
2022-05-06 00:24:36 -0400

    Adds img and stying to async loading view

--- src/Components/LoadingView.js
+++ src/Components/LoadingView.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import LoadingTransactions from '../Assets/Fetching_Transactions.jpg';
 import './LoadingView.scss';
 
 const LoadingView = () => {
     return (
         <div className='loadingView'>
-            <h1>Loading Transactions...</h1>
+            <h1>Getting Transactions...</h1>
+            <img className='loadingView__money' src={LoadingTransactions} alt='Money with wings' />
         </div>
     )
 }
