9db9296c198b65c14db20687c8630886f811cebb
Talia Pichardo <taliapichardo@gmail.com>
2022-05-06 00:25:27 -0400

    Uses framer motion to add styling to home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,23 +1,102 @@
 import React from 'react';
-import { Jumbotron, Container, Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import HomeImg from '../Assets/Home.jpg'
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
 
 const Home = () => {
+    const Section = styled.section`
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `
+
+    const Container = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    height: 100vh;
+    padding: 3rem calc((100vw-1300px) / 2);
+  `
+
+    const ColumnLeft = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    padding: 2rem 6rem;
+
+    h1{
+        margin-bottom: 0.5rem;
+        font-size: 3rem;
+    }
+
+    p{
+        margin: 2rem 0;
+        font-size: 2rem;
+        line-height; 1;
+    }
+    `
+
+    const Image = styled.img`
+    height: 490px;
+  `
+
+    const ColumnRight = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 60px;
+  `
+
+    const fadeLeft = {
+        hidden: { opacity: 0, x: -100 },
+        visible: { opacity: 1, x: 0 }
+    };
+
     return (
-        <Container>
-            <Jumbotron className='text-center bg-warning mt-5'>
-                <h1>Welcome to Bread!</h1>
-                <p>Stuck between <strong>"I need to save money"</strong> and <strong>"you only live once"</strong>?</p>
-                <h6>Start budgeting your money today with Bread, the best budgeting app!</h6>
-                <br />
-                <Link to='/transactions/new'>
-                    <Button>
-                        Add New Transaction
-                    </Button>
-                </Link>
-            </Jumbotron>
-        </Container>
-    );
+        <Section>
+            <Container>
+                <ColumnLeft id='bk-text'>
+                    <motion.h1
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ duration: 2 }}
+                    >
+                        Welcome to Bread!
+                    </motion.h1>
+                    <motion.p
+                        variants={fadeLeft}
+                        initial='hidden'
+                        animate='visible'
+                        transition={{ duration: 3 }}
+                        id='quote-font'
+                    >
+                        Stuck between <strong>"I need to save money"</strong> and <strong>"you only live once"</strong>?
+                    </motion.p>
+                    <motion.p
+                        variants={fadeLeft}
+                        initial='hidden'
+                        animate='visible'
+                        transition={{ duration: 3 }}
+                        id='quote-font'
+                    >
+                        Start budgeting your money today with Bread, the best budgeting app!
+                    </motion.p>
+                    <Link to='/transactions/new'>
+                        <Button>
+                            Add Transaction
+                        </Button>
+                    </Link>
+                </ColumnLeft>
+                <ColumnRight>
+                    <Image src={HomeImg} alt='two people exercising' />
+                </ColumnRight>
+            </Container>
+        </Section>
+    )
 };
 
 export default Home;
