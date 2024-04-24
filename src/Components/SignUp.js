fc1d4de8db058aecf8fdc9eea80297d445b519e4
Talia Pichardo <taliapichardo@gmail.com>
2024-04-24 01:00:15 -0400

    update to using navigate API instead of history

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,115 +1,133 @@
-import React from 'react';
-import { useRef, useState } from 'react';
-import { useAuth } from '../context/AuthContext.js';
-import { Link, useHistory } from 'react-router-dom';
-import { Card, Form, Button, Alert, Container } from 'react-bootstrap';
-import { auth } from '../util/firebaseConfig.js'
-import { updateProfile } from 'firebase/auth';
-import './SignUp.scss';
-
-
+import React from "react";
+import { useRef, useState } from "react";
+import { useAuth } from "../context/AuthContext.js";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom-v5-compat";
+import { Card, Form, Button, Alert, Container } from "react-bootstrap";
+import { auth } from "../util/firebaseConfig.js";
+import { updateProfile } from "firebase/auth";
+import "./SignUp.scss";
 
 const SignUp = () => {
-    let emailRef = useRef();
-    let passwordRef = useRef();
-    let confirmPasswordRef = useRef();
-    let nameRef = useRef();
-    const [error, setError] = useState('');
-    const [loading, setLoading] = useState(false);
-    const { signUp } = useAuth();
-    let history = useHistory();
+	let emailRef = useRef();
+	let passwordRef = useRef();
+	let confirmPasswordRef = useRef();
+	let nameRef = useRef();
+	const [error, setError] = useState("");
+	const [loading, setLoading] = useState(false);
+	const { signUp } = useAuth();
+	const navigate = useNavigate();
 
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
+		if (passwordRef.current.value !== confirmPasswordRef.current.value) {
+			return setError("Passwords do not match.");
+		}
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+		try {
+			setError("");
+			setLoading(true);
+			await signUp(emailRef.current.value, passwordRef.current.value).then(
+				() => {
+					updateProfile(auth.currentUser, {
+						displayName: nameRef.current.value,
+					});
+				}
+			);
+			navigate("/signin");
+		} catch (error) {
+			const { code } = error;
 
-        if (passwordRef.current.value !== confirmPasswordRef.current.value) {
-            return setError('Passwords do not match.');
-        };
+			if (code === "auth/invalid-email") {
+				setError("Unable to create an account. The provided email is invalid.");
+			} else if (code === "auth/email-already-in-use") {
+				setError(
+					"Unable to create an account. The provided email is already in use."
+				);
+			} else {
+				const fireErrorMessage = error.message
+					.split(" ")
+					.filter((word) => {
+						return (
+							word !== "Firebase:" &&
+							word !== "(auth/weak-password)." &&
+							word !== "(auth/email-already-exists)."
+						);
+					})
+					.join(" ");
 
-        try {
-            setError('');
-            setLoading(true);
-            await signUp(emailRef.current.value, passwordRef.current.value)
-                .then(() => {
-                    updateProfile(auth.currentUser, {
-                        displayName: nameRef.current.value
-                    })
-                })
-            history.push('/signin')
-        } catch (error) {
-            const { code } = error;
+				setError(`Unable to create an account. ${fireErrorMessage}.`);
+			}
+		}
+		setLoading(false);
+	};
 
-            if (code === 'auth/invalid-email') {
-                setError('Unable to create an account. The provided email is invalid.');
-            } else if (code === 'auth/email-already-in-use') {
-                setError('Unable to create an account. The provided email is already in use.');
-            } else {
-                const fireErrorMessage = error.message.split(' ').filter(word => {
-                    return word !== 'Firebase:' &&
-                        word !== '(auth/weak-password).' &&
-                        word !== '(auth/email-already-exists).'
-                }).join(' ');
-
-                setError(`Unable to create an account. ${fireErrorMessage}.`);
-            }
-        }
-        setLoading(false);
-    };
-
-    return (
-        <Container>
-            <Card className='signUp'>
-                <Card.Body>
-                    <h3 className='signUp__greeting'>Sign Up</h3>
-                    {error && <Alert variant='danger'>{error}</Alert>}
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group controlId='displayName'>
-                            <Form.Label>Name:</Form.Label>
-                            <Form.Control
-                                type='text'
-                                ref={nameRef}
-                                placeholder="name"
-                                required
-                            />
-                        </Form.Group>
-                        <Form.Group controlId='email'>
-                            <Form.Label>Email:</Form.Label>
-                            <Form.Control
-                                type='email'
-                                ref={emailRef}
-                                placeholder="example@gmail.com"
-                                required
-                            />
-                        </Form.Group>
-                        <Form.Group controlId='password'>
-                            <Form.Label>Password:</Form.Label>
-                            <Form.Control
-                                type='password'
-                                ref={passwordRef}
-                                placeholder='password must be at least 6 characters'
-                                required
-                            />
-                        </Form.Group>
-                        <Form.Group controlId='confirmPassword'>
-                            <Form.Label>Confirm Password:</Form.Label>
-                            <Form.Control
-                                type='password'
-                                ref={confirmPasswordRef}
-                                placeholder="confirm password"
-                                required
-                            />
-                        </Form.Group>
-                        <div className="signUp__BtnDiv">
-                            <Button className='signUp__Btn' disabled={loading} type='submit' variant='primary'>Sign Up</Button>
-                        </div>
-                    </Form>
-                    <p className='signUp__signIn'>Already have an account? <Link to='/signin' className='authenticate'>Sign in</Link></p>
-                </Card.Body>
-            </Card>
-        </Container>
-    );
-}
+	return (
+		<Container>
+			<Card className="signUp">
+				<Card.Body>
+					<h3 className="signUp__greeting">Sign Up</h3>
+					{error && <Alert variant="danger">{error}</Alert>}
+					<Form onSubmit={handleSubmit}>
+						<Form.Group controlId="displayName">
+							<Form.Label>Name:</Form.Label>
+							<Form.Control
+								type="text"
+								ref={nameRef}
+								placeholder="name"
+								required
+							/>
+						</Form.Group>
+						<Form.Group controlId="email">
+							<Form.Label>Email:</Form.Label>
+							<Form.Control
+								type="email"
+								ref={emailRef}
+								placeholder="example@gmail.com"
+								required
+							/>
+						</Form.Group>
+						<Form.Group controlId="password">
+							<Form.Label>Password:</Form.Label>
+							<Form.Control
+								type="password"
+								ref={passwordRef}
+								placeholder="password must be at least 6 characters"
+								required
+							/>
+						</Form.Group>
+						<Form.Group controlId="confirmPassword">
+							<Form.Label>Confirm Password:</Form.Label>
+							<Form.Control
+								type="password"
+								ref={confirmPasswordRef}
+								placeholder="confirm password"
+								required
+							/>
+						</Form.Group>
+						<div className="signUp__BtnDiv">
+							<Button
+								className="signUp__Btn"
+								disabled={loading}
+								type="submit"
+								variant="primary">
+								Sign Up
+							</Button>
+						</div>
+					</Form>
+					<p className="signUp__signIn">
+						Already have an account?{" "}
+						<Link
+							to="/signin"
+							className="authenticate">
+							Sign in
+						</Link>
+					</p>
+				</Card.Body>
+			</Card>
+		</Container>
+	);
+};
 
 export default SignUp;
--- src/Components/TransactionDetails.js
+++ src/Components/TransactionDetails.js
@@ -1,76 +1,86 @@
-import React from 'react';
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-import { Link, useParams, useHistory, withRouter, Redirect } from 'react-router-dom';
-import { apiURL } from '../util/apiURL';
-import { useAuth } from '../context/AuthContext'
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Link, useParams, withRouter, Redirect } from "react-router-dom";
+import { useNavigate } from "react-router-dom-v5-compat";
+import { apiURL } from "../util/apiURL";
+import { useAuth } from "../context/AuthContext";
 
-import { BsTrash } from 'react-icons/bs'
-import { GrEdit } from 'react-icons/gr';
+import { BsTrash } from "react-icons/bs";
+import { GrEdit } from "react-icons/gr";
 
-import { Button, Card, Container } from 'react-bootstrap';
-import './TransactionDetails.scss';
-
+import { Button, Card, Container } from "react-bootstrap";
+import "./TransactionDetails.scss";
 
 const API = apiURL();
 
 function TransactionDetails() {
+	const [transaction, setTransaction] = useState([]);
+	const navigate = useNavigate();
+	const { id } = useParams();
+	const { currentUser } = useAuth();
 
-    const [transaction, setTransaction] = useState([]);
-    let history = useHistory();
-    const { id } = useParams();
-    const { currentUser } = useAuth();
+	useEffect(() => {
+		axios
+			.get(`${API}/transactions/${id}`)
+			.then((response) => {
+				const { data } = response;
+				setTransaction(data);
+			})
+			.catch((e) => {
+				console.log(e);
+				navigate("/not-found");
+			});
+	}, [id]);
 
-    useEffect(() => {
-        axios.get(`${API}/transactions/${id}`)
-            .then((response) => {
-                const { data } = response
-                setTransaction(data);
-            }).catch((e) => {
-                console.log(e)
-                history.push('/not-found')
-            })
-    }, [id, history])
+	const deleteTransaction = async (id) => {
+		try {
+			await axios.delete(`${API}/transactions/${id}`);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
+	const handleDelete = async () => {
+		try {
+			await deleteTransaction(id);
+			navigate("/transactions");
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-    const deleteTransaction = async (id) => {
-        try {
-            await axios.delete(`${API}/transactions/${id}`);
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    const handleDelete = async () => {
-        await deleteTransaction(id);
-        history.push('/transactions');
-    };
-
-    return (
-        <Container>
-            {!currentUser && <Redirect to='/signin' />}
-            <Card className='col-sm-5 mt-5 mx-auto transaction'>
-                <Card.Body>
-                    <Card.Title>{transaction.transaction_name}</Card.Title>
-                    {transaction.transaction_type === "Expense" ? <Card.Text className="text-danger">Expense: {transaction.transaction_amount}</Card.Text> : <Card.Text className="text-success">Deposit: {transaction.transaction_amount}</Card.Text>}
-                    <Card.Text>
-                        From: {transaction.transaction_vendor}
-                    </Card.Text>
-                    <Link to={`/transactions/${id}/edit`}>
-                        <Button variant='outline-success'>
-                            <GrEdit />
-                        </Button>
-                    </Link>
-                    <Button variant='outline-danger'>
-                        <BsTrash onClick={handleDelete} />
-                    </Button>
-                    <Link to={'/transactions'}>
-                        <Button variant='outline-primary'>Back</Button>
-                    </Link>
-                </Card.Body>
-            </Card>
-        </Container>
-    )
+	return (
+		<Container>
+			{!currentUser && <Redirect to="/signin" />}
+			<Card className="col-sm-5 mt-5 mx-auto transaction">
+				<Card.Body>
+					<Card.Title>{transaction.transaction_name}</Card.Title>
+					{transaction.transaction_type === "Expense" ? (
+						<Card.Text className="text-danger">
+							Expense: {transaction.transaction_amount}
+						</Card.Text>
+					) : (
+						<Card.Text className="text-success">
+							Deposit: {transaction.transaction_amount}
+						</Card.Text>
+					)}
+					<Card.Text>From: {transaction.transaction_vendor}</Card.Text>
+					<Link to={`/transactions/${id}/edit`}>
+						<Button variant="outline-success">
+							<GrEdit />
+						</Button>
+					</Link>
+					<Button variant="outline-danger">
+						<BsTrash onClick={handleDelete} />
+					</Button>
+					<Link to={"/transactions"}>
+						<Button variant="outline-primary">Back</Button>
+					</Link>
+				</Card.Body>
+			</Card>
+		</Container>
+	);
 }
 
 export default withRouter(TransactionDetails);
